71ac28d832e5557fc3ca99b94a5778f80834c169
Luana Silva <luanasilva.ss9497@gmail.com>
2024-03-18 20:18:52 -0300

    first commit

---
+++ src/Formulario.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Formulario extends React.Component {
+
+
+    render() {
+        return (
+            <form onSubmit={this.props.funcaoBotao} >
+                <input type="text" value={this.props.campoNome} placeholder="Informe o nome" className="form-control" name="nome" onChange={this.props.funcaoCampo} />
+                <input type="number" value={this.props.campoIdade} placeholder="Informe a idade" className="form-control" name="idade" onChange={this.props.funcaoCampo} />
+                <input type="submit" value="Cadastrar" className="btn btn-primary" />
+            </form>
+        );
+
+    }
+}
+export default Formulario;
---
+++ src/principal.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+
+import Formulario from './Formulario';
+
+import Tabela from './Tabela';
+//Componente
+class Principal extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state ={
+            nome : '',
+            idade : '',
+            vetor : []
+        }
+    }
+// onChange dinamico
+
+    aoDigitar = (campo) => {
+        this.setState({[campo.target.name] : campo.target.value});
+    }
+
+    aoClicar = (botao) => {
+        botao.preventDefault();
+        
+        var copiaVetor = [...this.state.vetor];
+
+        copiaVetor.push({
+            'nome' : this.state.nome,
+            'idade' : this.state.idade
+        });
+
+        this.setState({vetor : copiaVetor});
+
+        this.setState({
+            nome : '',
+            idade : ''
+        })
+    }
+
+    //render
+    render(){
+        return(<div>
+           <Formulario  campoNome={this.state.nome}  campoIdade= {this.state.idade} funcaoBotao={this.aoClicar} funcaoCampo={this.aoDigitar} />
+           <Tabela dados={this.state.vetor} />
+        </div> 
+        );
+    }
+}
+
+export default Principal;
